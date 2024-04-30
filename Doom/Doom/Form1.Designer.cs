d2f94f6fc3060233c66a1946034d0b16d09565c1
Sorokin Iury <144129825+akiraduck@users.noreply.github.com>
2024-04-30 19:31:46 +0500

    Add files via upload
    доделал всю логику игры, осталость только разбить по MVC и норм, я думаю

---
+++ Doom/Doom/Form1.Designer.cs
@@ -0,0 +1,131 @@
+﻿namespace Doom
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtFood = new System.Windows.Forms.Label();
+            this.txtScore = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.healthBar = new System.Windows.Forms.ProgressBar();
+            this.player = new System.Windows.Forms.PictureBox();
+            this.GameTimer = new System.Timers.Timer();
+            ((System.ComponentModel.ISupportInitialize)(this.player)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.GameTimer)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtFood
+            // 
+            this.txtFood.Font = new System.Drawing.Font("Consolas", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.txtFood.ForeColor = System.Drawing.Color.Black;
+            this.txtFood.Location = new System.Drawing.Point(20, 18);
+            this.txtFood.Name = "txtFood";
+            this.txtFood.Size = new System.Drawing.Size(151, 36);
+            this.txtFood.TabIndex = 0;
+            this.txtFood.Text = "Еда: 0";
+            // 
+            // txtScore
+            // 
+            this.txtScore.Font = new System.Drawing.Font("Consolas", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.txtScore.ForeColor = System.Drawing.Color.Black;
+            this.txtScore.Location = new System.Drawing.Point(232, 18);
+            this.txtScore.Name = "txtScore";
+            this.txtScore.Size = new System.Drawing.Size(212, 36);
+            this.txtScore.TabIndex = 1;
+            this.txtScore.Text = "Убийства: 0";
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Consolas", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.ForeColor = System.Drawing.Color.Black;
+            this.label1.Location = new System.Drawing.Point(495, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(151, 36);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Здоровье:";
+            // 
+            // healthBar
+            // 
+            this.healthBar.Location = new System.Drawing.Point(657, 21);
+            this.healthBar.Name = "healthBar";
+            this.healthBar.Size = new System.Drawing.Size(250, 32);
+            this.healthBar.TabIndex = 3;
+            this.healthBar.Value = 100;
+            // 
+            // player
+            // 
+            this.player.Image = global::Doom.Properties.Resources.right;
+            this.player.Location = new System.Drawing.Point(470, 564);
+            this.player.Name = "player";
+            this.player.Size = new System.Drawing.Size(60, 60);
+            this.player.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.player.TabIndex = 4;
+            this.player.TabStop = false;
+            // 
+            // GameTimer
+            // 
+            this.GameTimer.Enabled = true;
+            this.GameTimer.Interval = 20D;
+            this.GameTimer.SynchronizingObject = this;
+            this.GameTimer.Elapsed += new System.Timers.ElapsedEventHandler(this.MainTimerEvent);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(984, 661);
+            this.Controls.Add(this.player);
+            this.Controls.Add(this.healthBar);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtScore);
+            this.Controls.Add(this.txtFood);
+            this.Name = "Form1";
+            this.Text = "HTML Shooter";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.KeyIsDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.KeyIsUp);
+            ((System.ComponentModel.ISupportInitialize)(this.player)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.GameTimer)).EndInit();
+            this.ResumeLayout(false);
+        }
+
+        private System.Timers.Timer GameTimer;
+
+        private System.Windows.Forms.PictureBox player;
+
+        private System.Windows.Forms.ProgressBar healthBar;
+
+        private System.Windows.Forms.Label label1;
+
+        private System.Windows.Forms.Label txtFood;
+
+        private System.Windows.Forms.Label txtScore;
+
+        #endregion
+    }
+}
---
+++ Doom/Doom/Form1.cs
@@ -0,0 +1,278 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Resources;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+using System.Windows.Forms;
+using Doom.Properties;
+
+namespace Doom
+{
+    public partial class Form1 : Form
+    {
+        private bool goUp, goDown, goRight, goLeft, gameOver;
+        private string facing = "up";
+        private int playerHealth = 100;
+        private int speed = 9;
+        private int food = 10;
+        private int score;
+        private int enemySpeed = 2;
+        private Random randomNumber = new Random();
+        private List<PictureBox> enemiesList = new List<PictureBox>();
+        
+        public Form1()
+        {
+            InitializeComponent();
+            RestartGame();
+        }
+
+        private void MainTimerEvent(object sender, ElapsedEventArgs e)
+        {
+            if (playerHealth > 1)
+            {
+                healthBar.Value = playerHealth;
+            }
+
+            else
+            {
+                gameOver = true;
+                player.Image = Resources.dead;
+                GameTimer.Stop();
+            }
+            
+            txtFood.Text = "Еда: " + food;
+            txtScore.Text = "Убийства: " + score;
+            
+            if (goLeft && player.Left > 0)
+            {
+                player.Left -= speed;
+            }
+
+            if (goRight && player.Left + player.Width < ClientSize.Width)
+            {
+                player.Left += speed;
+            }
+            
+            if (goUp && player.Top > 70)
+            {
+                player.Top -= speed;
+            }
+            
+            if (goDown && player.Top + player.Height < ClientSize.Height)
+            {
+                player.Top += speed;
+            }
+
+            foreach (Control x in Controls)
+            {
+                if (x is PictureBox && (string)x.Tag == "food")
+                {
+                    if (player.Bounds.IntersectsWith(x.Bounds))
+                    {
+                        Controls.Remove(x);
+                        ((PictureBox)x).Dispose();
+                        food += 6;
+                    }
+                }
+            }
+            
+            foreach (Control x in Controls)
+            {
+                if (x is PictureBox && (string)x.Tag == "ammo")
+                {
+                    if (player.Bounds.IntersectsWith(x.Bounds))
+                    {
+                        Controls.Remove(x);
+                        ((PictureBox)x).Dispose();
+                        food += 6;
+                    }
+                }
+
+                if (x is PictureBox && (string)x.Tag == "enemy")
+                {
+                    if (player.Bounds.IntersectsWith(x.Bounds))
+                    {
+                        playerHealth -= 1;
+                    }
+                    
+                    if (x.Left > player.Left)
+                    {
+                        x.Left -= enemySpeed; 
+                        ((PictureBox)x).Image = Resources.enemy_left;
+                    }
+                        
+                    if (x.Left < player.Left)
+                    {
+                        x.Left += enemySpeed;
+                        ((PictureBox)x).Image = Resources.enemy_right;
+                    }
+
+                    if (x.Top > player.Top)
+                        x.Top -= enemySpeed;
+                    
+                    if (x.Top < player.Top)
+                        x.Top += enemySpeed;
+                }
+                
+                foreach (Control j in Controls)
+                {
+                    if (j is PictureBox && (string)j.Tag == "bullet" && x is PictureBox && (string)x.Tag == "enemy")
+                    {
+                        if (x.Bounds.IntersectsWith(j.Bounds))
+                        {
+                            score++;
+                            Controls.Remove(j);
+                            ((PictureBox)j).Dispose();
+                            Controls.Remove(x);
+                            enemiesList.Remove((PictureBox)x);
+                            MakeEnemies();
+                        }
+                    }
+                }
+            }
+        }
+
+        private void KeyIsDown(object sender, KeyEventArgs e)
+        {
+            if (gameOver)
+            {
+                return;
+            }
+            
+            if (e.KeyCode == Keys.Left)
+            {
+                goLeft = true;
+                facing = "left";
+                player.Image = Resources.left;
+            }
+            
+            if (e.KeyCode == Keys.Right)
+            {
+                goRight = true;
+                facing = "right";
+                player.Image = Resources.right;
+            }
+            
+            if (e.KeyCode == Keys.Up)
+            {
+                goUp = true;
+                facing = "up";
+            }
+            
+            if (e.KeyCode == Keys.Down)
+            {
+                goDown = true;
+                facing = "down";
+            }
+        }
+
+        private void KeyIsUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Left)
+            {
+                goLeft = false;
+            }
+                        
+            if (e.KeyCode == Keys.Right)
+            {
+                goRight = false;
+            }
+                        
+            if (e.KeyCode == Keys.Up)
+            {
+                goUp = false;
+            }
+                        
+            if (e.KeyCode == Keys.Down)
+            {
+                goDown = false;
+            }
+            
+            if (e.KeyCode == Keys.Space && food > 0 && !gameOver)
+            {
+                food--;
+                ShootBullet(facing);
+
+                if (food < 1)
+                {
+                    DropFood();
+                }
+            }
+
+            if (e.KeyCode == Keys.Enter && gameOver)
+            {
+                RestartGame();
+            }
+        }
+        
+        private void ShootBullet(string direction)
+        {
+            var shootBullet = new Bullet();
+            shootBullet.direction = direction;
+            shootBullet.bulletLeft = player.Left + (player.Width / 2);
+            shootBullet.bulletTop = player.Top + (player.Height / 2);
+            shootBullet.MakeBullet(this);
+        }
+        
+        private void MakeEnemies()
+        {
+            var enemy = new PictureBox();
+            enemy.Tag = "enemy";
+            enemy.Image = Resources.enemy_left;
+            enemy.Left = randomNumber.Next(0, 1000);
+            enemy.Top = randomNumber.Next(70, 700);
+            enemy.SizeMode = PictureBoxSizeMode.AutoSize;
+            enemiesList.Add(enemy);
+            Controls.Add(enemy);
+            player.BringToFront();
+        }
+
+        private void DropFood()
+        {
+            var food = new PictureBox();
+            food.Image = Resources.food;
+            food.SizeMode = PictureBoxSizeMode.AutoSize;
+            food.Left = randomNumber.Next(10, ClientSize.Width - food.Width);
+            food.Top = randomNumber.Next(80, ClientSize.Height - food.Height);
+            food.Tag = "food";
+            Controls.Add(food);
+            
+            food.BringToFront();
+            player.BringToFront();
+        }
+        
+        private void RestartGame()
+        {
+            player.Image = Resources.right;
+
+            foreach (var i in enemiesList)
+            {
+                Controls.Remove(i);
+            }
+            
+            enemiesList.Clear();
+
+            for (var i = 0; i < 3; i++)
+            {
+                MakeEnemies();
+            }
+
+            goUp = false;
+            goDown = false;
+            goLeft = false;
+            goRight = false;
+            gameOver = false;
+
+            playerHealth = 100;
+            score = 0;
+            food = 10;
+            
+            GameTimer.Start();
+        }
+    }
+}
